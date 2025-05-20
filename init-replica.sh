d3762d36f960556ed235caac62ebd8a8019f61df
Lee Ryan <rsl82@rutgers.edu>
2025-05-20 13:16:15 +0900

    fix: primary 노드 지정해 오류 수정

--- init-replica.sh
+++ init-replica.sh
@@ -22,9 +22,9 @@ else
     rs.initiate({
       _id: "rs0",
       members: [
-        { _id: 0, host: "mongodb:27017" },
-        { _id: 1, host: "mongodb2:27017" },
-        { _id: 2, host: "mongodb3:27017" }
+        { _id: 0, host: "mongodb:27017", priority: 10 },
+        { _id: 1, host: "mongodb2:27017", priority: 1 },
+        { _id: 2, host: "mongodb3:27017", priority: 1 }
       ]
     });
   '
